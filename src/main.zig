1efbc52039592c0bb5bcc10b8ccd56f75e3117ee
Omar AlSuwaidi <omar.ali.alsuwaidi@gmail.com>
2024-04-01 12:56:57 +0400

    added comments about variable passing

--- src/main.zig
+++ src/main.zig
@@ -62,7 +62,7 @@ pub fn main() !void {
     print("some_char is: {}\n", .{some_char}); // by default, it prints its Unicode encoding (65 in this case)
     print("some_char is now: {c}\n", .{some_char});
 
-    var byte_array: [3]u8 = .{ 'h', 'e', 'y' }; // initialize a mutable array of three, single byte (8bit) characters
+    var byte_array: [3]u8 = .{ 'h', 'e', 'y' }; // initialize a mutable array of three, single byte (8-bit) characters
     byte_array[0] = 'x';
     print("byte_array is now: {s}\n\n", .{byte_array});
 
@@ -84,32 +84,34 @@ pub fn main() !void {
 
     // pointers:
     var some_int: i32 = 100;
-    const ptr = &some_int; // type "*i32"
+    const ptr = &some_int; // return the address of the variable which in this case is of type "*i32"
     print("The address of some_int in memory is: {},\nwith the pointer pointing to the value: {}\n\n", .{ ptr, ptr.* }); // dereference the pointer via ".*"
 
     // What's the point (use) of pointers?
-    var var1: f32 = 0;
-    var var2: f32 = 0;
-    print("Original values of var1 and var2 are: {d}, {d}\n", .{ var1, var2 });
+    var float1: f32 = 0;
+    var float2: f32 = 0;
+    print("Original values of float1 and float2 are: {d}, {d}\nwith their stored addresses at: {}, {}.\n\n", .{ float1, float2, &float1, &float2 });
 
-    pass_by_value_manipulation(var1, var2);
-    print("After first function, values of var1 and var2 are still: {}, {}\n", .{ var1, var2 });
+    pass_by_value_manipulation(float1, float2);
+    print("After first function, values of float1 and float2 are still: {d}, {d}\n\n", .{ float1, float2 });
 
-    pass_by_reference_manipulation(&var1, &var2); // pass the variables' memory addresses rather than their values!
-    print("After second function, values of var1 and var2 are now: {d}, {d}\n\n", .{ var1, var2 });
+    pass_by_reference_manipulation(&float1, &float2); // pass the variables' memory addresses rather than their values!
+    print("After second function, values of float1 and float2 are now: {d}, {d}\n", .{ float1, float2 });
 }
 
-// By default, arguments are passed by value into functions, meaning a *copy* of each argument is created when it's passed into a function
-fn pass_by_value_manipulation(a: f32, b: f32) void {
+// By default, arguments are passed by into functions by *value*, meaning a *copy* of each argument is created when it's passed into a function
+fn pass_by_value_manipulation(x: f32, y: f32) void {
+    print("Addresses of x (float1) and y (float2) are: {}, and {}, respectively.\nNotice that the addresses are different than the original variables because they're a copy!\n", .{ &x, &y });
     // In Zig a function's arguments are immutable by default, hence you cannot directly modify their values (unlike in C)!
     // a += 20;  -> error: cannot assign to constant
     // b = 10;  --> error: cannot assign to constant
-    _ = a + b; // must use the function's arguments
+    // _ = a + b; // must use the function's arguments
 }
 
-fn pass_by_reference_manipulation(a: *f32, b: *f32) void {
+fn pass_by_reference_manipulation(x: *f32, y: *f32) void {
+    print("Addresses of x (float1) and y (float2) are: {}, and {}, respectively.\nNotice that now since we're using pointers, they're the same addresses as the original varaibles!\n", .{ x, y });
     // Access the variables' original value in memory by dereferencing the pointers pointing to their address in memory
-    a.* = 99;
-    b.* = 10;
+    x.* = 99;
+    y.* = 10;
     return undefined; // "undefined" is the same as "void", but "null" is a value, it represents the *absence* of a value!
 }
